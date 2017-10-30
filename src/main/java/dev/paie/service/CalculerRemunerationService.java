b8738eb13455bf7bdfd9c7ab425f35500182df55
CHAFFARD Joris <chaffard.joris@outlook.fr>
2017-10-30 22:41:02 +0100

    fin tp2

---
+++ src/main/java/dev/paie/service/CalculerRemunerationService.java
@@ -0,0 +1,8 @@
+
+
+package dev.paie.service;
+import dev.paie.entite.*;
+
+public interface CalculerRemunerationService {
+	ResultatCalculRemuneration calculer(BulletinSalaire bulletin);
+}
---
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package dev.paie.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.paie.entite.BulletinSalaire;
+import dev.paie.entite.Cotisation;
+import dev.paie.entite.ResultatCalculRemuneration;
+import dev.paie.util.PaieUtils;
+
+/**
+ * @author joris
+ *
+ */
+@Service
+public class CalculerRemunerationServiceSimple implements CalculerRemunerationService {
+	
+	@Autowired private PaieUtils paieUtils;
+
+	/* (non-Javadoc)
+	 * @see dev.paie.service.CalculerRemunerationService#calculer(dev.paie.entite.BulletinSalaire)
+	 */
+	@Override
+	public ResultatCalculRemuneration calculer(BulletinSalaire bulletin) {
+		
+		ResultatCalculRemuneration resultat=new ResultatCalculRemuneration();
+		
+		BigDecimal salaireBase = bulletin.getRemunerationEmploye().getGrade().getNbHeuresBase().multiply
+				(bulletin.getRemunerationEmploye().getGrade().getTauxBase());
+		BigDecimal salaireBrut = salaireBase.add(bulletin.getPrimeExceptionnelle());
+			
+		
+		resultat.setSalaireBrut(paieUtils.formaterBigDecimal(salaireBrut));
+		
+		salaireBrut = new BigDecimal(resultat.getSalaireBrut());
+		
+		BigDecimal non_imposable= new BigDecimal(0);
+		BigDecimal imposable= new BigDecimal(0);
+		List<Cotisation> l = bulletin.getRemunerationEmploye().getProfilRemuneration().getCotisationsNonImposables();
+		for(Cotisation c:l){
+			if(c.getTauxSalarial()!=null){
+				non_imposable=non_imposable.add(c.getTauxSalarial().multiply(salaireBrut));
+			}
+			
+			if(c.getTauxPatronal()!=null){
+				imposable=imposable.add(c.getTauxPatronal().multiply(salaireBrut));
+			}
+		}
+		
+		resultat.setTotalRetenueSalarial(paieUtils.formaterBigDecimal(non_imposable));
+	
+		resultat.setTotalCotisationsPatronales(paieUtils.formaterBigDecimal(imposable));
+		
+		BigDecimal netimposable = salaireBrut.add(new BigDecimal(resultat.getTotalRetenueSalarial()).negate());
+		
+		resultat.setNetImposable(paieUtils.formaterBigDecimal(netimposable));
+		
+		netimposable = new BigDecimal(resultat.getNetImposable());
+		
+		BigDecimal sommeImposable= new BigDecimal(0);
+		List<Cotisation> lImposable = bulletin.getRemunerationEmploye().getProfilRemuneration().getCotisationsImposables();
+		for(Cotisation c:lImposable){
+			if(c.getTauxSalarial()!=null){
+				sommeImposable=sommeImposable.add(c.getTauxSalarial().multiply(salaireBrut));
+			}
+			
+		}
+		
+		resultat.setNetAPayer(paieUtils.formaterBigDecimal(netimposable.add(sommeImposable.negate())));
+		
+		return resultat;
+	}
+
+
+}
